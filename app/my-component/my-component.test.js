ac3a012aa13186f8854247b34c55612bbd9d9f77
Frank Gutierrez <dearfrankg@gmail.com>
2023-06-08 01:59:18 -0700

    update test

--- app/my-component/my-component.test.js
+++ app/my-component/my-component.test.js
@@ -1,25 +1,27 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import MyComponent from "./page";
 import { useRouter } from "next/navigation";
+import MyComponent from "./page";
 
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
 }));
 
-const pushMock = jest.fn();
-
-useRouter.mockReturnValue({
-  push: pushMock,
-});
+const mockRouter = { push: jest.fn() };
 
 describe("MyComponent", () => {
-  test('navigates to "/users" route when button is clicked', () => {
+  beforeEach(() => {
+    useRouter.mockReturnValue(mockRouter);
+  });
+
+  test("renders component correctly", () => {
     render(<MyComponent />);
+    expect(screen.getByText("My Component")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
 
-    const button = screen.getByRole("button");
-    fireEvent.click(button);
-
-    expect(pushMock).toHaveBeenCalledWith("/users");
-    expect(pushMock).toHaveBeenCalledTimes(1);
+  test("navigates to /users on button click", () => {
+    render(<MyComponent />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(mockRouter.push).toHaveBeenCalledWith("/users");
   });
 });
