ceaab4ad8b4865d77d08e5fc05bbe327c913e2d7
Frank Gutierrez <dearfrankg@gmail.com>
2023-06-25 04:31:29 -0700

    add link

--- app/components/pages/demos-page/demos/shadcn-ui/shadcn-ui.tsx
+++ app/components/pages/demos-page/demos/shadcn-ui/shadcn-ui.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Image from "next/image";
+import { Button } from "antd";
 import { useGames } from "./query";
 import Loading from "./loading";
 
@@ -29,18 +30,25 @@ function App() {
     return <Loading />;
   }
 
+  const handleClick = () => {
+    window.open("https://ui.shadcn.com/", "_blank");
+  };
+
   return (
-    <main className="w-full m-24 rounded-md grid grid-cols-4 gap-12">
-      {games.map(game => (
-        <div key={game.id} className="col-span-4 2xl:col-span-2">
-          <h1 className="text-lg font-medium">{game.name}</h1>
-          <p className="font-normal">Rating: {game.rating}</p>
-          <div className="aspect-video relative">
-            <Image src={game.background_image} fill className="object-cover rounded-md" alt={game.name} />
+    <>
+      <Button onClick={handleClick}>https://ui.shadcn.com/</Button>
+      <main className="w-full m-24 rounded-md grid grid-cols-4 gap-12">
+        {games.map(game => (
+          <div key={game.id} className="col-span-4 2xl:col-span-2">
+            <h1 className="text-lg font-medium">{game.name}</h1>
+            <p className="font-normal">Rating: {game.rating}</p>
+            <div className="aspect-video relative">
+              <Image src={game.background_image} fill className="object-cover rounded-md" alt={game.name} />
+            </div>
           </div>
-        </div>
-      ))}
-    </main>
+        ))}
+      </main>
+    </>
   );
 }
 
